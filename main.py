2a8722b2626c4e81a78167613cd28c1ea4a64d86
naodtesfaye939-del <naodtesfaye939@gmail.com>
2026-03-10 06:10:35 +0300

    main.py

--- main.py
+++ main.py
@@ -81,7 +81,3 @@ def download(job_id):
 @app.route('/')
 def home():
     return jsonify({'status': 'ContentAI server is running!'})
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
